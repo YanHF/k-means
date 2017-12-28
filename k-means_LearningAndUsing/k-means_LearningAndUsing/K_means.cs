aa7efd0a87b93c5a5718ee61fdb62a730be457e6
MikoWoo <947483235@qq.com>
2017-12-28 14:13:32 +0800

    K-means算法简单学习与体验

---
+++ k-means_LearningAndUsing/k-means_LearningAndUsing/K_means.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    /// <summary>
+    /// author yanhf
+    /// K-Means
+    /// </summary>
+    public class K_Means
+    {
+        /// <summary>
+        /// Update index cluster identification based on cluster point key
+        /// </summary>
+        /// <param name="data">Data</param>
+        /// <param name="index">index</param>
+        /// <param name="key">key values</param>
+        public static void _K_Means(double[] data, ref int[] index, int[] key)
+        {
+            bool change = true;
+            int _index = 0;
+            double Length = 0;
+            while (change)
+            {
+                change = false;
+                for (int i = 0; i < data.Length; i++)//根据当前的聚簇点更新index聚簇标识数组
+                {
+                    for (int j = 0; j < key.Length; j++)
+                    {
+                        double currentLength = Math.Abs(data[key[j]] - data[i]);
+                        if (j == 0 || Length > currentLength)
+                        {
+                            Length = Math.Abs(data[key[j]] - data[i]);
+                            _index = j + 1;
+                        }
+                    }
+                    index[i] = _index;
+                    Length = 0;
+                }
+                change = _ChangeKey(data, index, ref key);
+            }
+        }
+        /// <summary>
+        /// Judging the current Key needs not to be changed
+        /// </summary>
+        /// <param name="data">Data</param>
+        /// <param name="index">index</param>
+        /// <param name="key">key values</param>
+        /// <returns></returns>
+        public static bool _ChangeKey(double[] data, int[] index, ref int[] key)
+        {
+            bool IsChange = false;
+            int i = 0;
+            List<double> _listData = new List<double>();
+            while (i++ < key.Length)
+            {
+                _listData.Clear();
+                for (int j = 0; j < index.Length; j++)
+                {
+                    if (index[j] == i)
+                    {
+                        _listData.Add(data[j]);
+                    }
+                }
+                _listData.Sort();
+                if (_listData.Count > 1)
+                {
+                    if (key[i - 1] != _index(data, _listData[_listData.Count / 2]))
+                    {
+                        IsChange = true;
+                        key[i - 1] = _index(data, _listData[_listData.Count / 2]);
+                    }
+                }
+            }
+            return IsChange;
+        }
+        /// <summary>
+        /// Get the index in the key's data
+        /// </summary>
+        /// <param name="data">Data</param>
+        /// <param name="num">key values</param>
+        /// <returns></returns>
+        public static int _index(double[] data, double num)
+        {
+            List<double> _listData = new List<double>(data);
+            return _listData.IndexOf(num) >= 0 ? _listData.IndexOf(num) : -1;
+        }
+    }
+}
--- k-means_LearningAndUsing/k-means_LearningAndUsing/Program.cs
+++ k-means_LearningAndUsing/k-means_LearningAndUsing/Program.cs
@@ -1,15 +1,68 @@
-﻿using System;
+﻿using DataStructure;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace k_means_LearningAndUsing
+namespace ClusterNumeric
 {
-    class Program
+    class ClusterNumProgram
     {
+        public static int dataNumber = 0;
+        public static int keyNumber = 0;
         static void Main(string[] args)
         {
+            start:
+            string[] inputArray = null;
+            {
+                Console.WriteLine("\nEnter the total number of data you want to divide and The number of divisions\nFor example: 100,3");
+                string input = Console.ReadLine();
+                inputArray = input.Split(new char[] { ',' });
+            } while (!int.TryParse(inputArray[0], out dataNumber) || !int.TryParse(inputArray[1], out keyNumber) || keyNumber >= dataNumber) ;
+
+            Random random = new Random();
+            double[] _data = new double[dataNumber];
+            Console.WriteLine("\nShow the total number of  random generated  data");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            int M = 1;
+            for (int i = 0; i < _data.Length; i++)
+            {
+                _data[i] = Math.Round(random.NextDouble() * (100 < dataNumber ? dataNumber : 100), 2);
+                Console.Write(_data[i].ToString("F2").PadLeft(7));
+                if (M++ % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            int[] _index = new int[dataNumber];
+            int[] keyData = new int[keyNumber];
+            for (int i = 0; i < keyNumber; i++)
+            {
+                keyData[i] = random.Next(0, dataNumber);
+            }
+            K_Means._K_Means(_data, ref _index, keyData);
+            Console.WriteLine("\n*************************************************************************");
+            Console.WriteLine("\nShow the Result");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            for (int i = 0; i < keyData.Length; i++)
+            {
+                M = 1;
+                Console.WriteLine("Value values of the " + (i + 1) + " group:key " + _data[keyData[i]].ToString().PadRight(7));
+                Console.WriteLine();
+                for (int j = 0; j < _index.Length; j++)
+                {
+                    if (_index[j] == i + 1)
+                    {
+                        Console.Write(_data[j].ToString("F2").PadLeft(7));
+                        if (M++ % 10 == 0)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("-------------------------------------------------------------------------");
+            }
+            Console.WriteLine("*************************************************************************");
+            goto start;
         }
     }
 }
